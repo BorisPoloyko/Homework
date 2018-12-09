54c881046fc9b28de4d93e8f5cb3258a81c80dd4
BorisPoloyko <43009854+BorisPoloyko@users.noreply.github.com>
2018-12-09 19:30:07 +0300

    Update 05.12.cpp

--- 05.12.cpp
+++ 05.12.cpp
@@ -3,15 +3,22 @@
 using namespace std;
 
 bool isLetter(char);
-void newStringOfWords(char*);
+void arrayOfWords(char*, char[100][100]);
 int getLength(char*);
-
+int substringPosition(char*, char*, int);
+void stringReplacement(char*, char*, char*, char*);
+void displayArray(char*);
 
 int main()
 {
-	
+	char string[25] = "Hello, hello, hetll";
+	char toBeReplaced[3] = "ll";
+	char replace[5] = "abc";
+	char newString[30] = { '\0' };
+	stringReplacement(string, toBeReplaced, replace, newString);
+	displayArray(newString);
 
-	
+	system("pause");
 }
 
 bool isLetter(char symbol)
@@ -26,18 +33,17 @@ bool isLetter(char symbol)
 int getLength(char* source)
 {
 	int length = 0;
-	while (source[length]) //ðàâíîñèëüíî  while(source[length]!= '\0')
+	while (source[length])
 	{
 		length++;
 	}
-
 	return length;
 }
 
-void newStringOfWords(char* string) 
+void arrayOfWords(char* string, char newString[100][100])
 {
-	char newString[100][100];
-	int nextLetter = 0, nextWord = 0;
+
+	int nextLetter = 0, nextWord = 0, flag = 0;
 	for (int i = 0; i < getLength(string); i++)
 	{
 		if (isLetter(string[i]))
@@ -53,5 +59,91 @@ void newStringOfWords(char* string)
 				nextLetter = 0;
 			}
 		}
+
+	}
+
+}
+
+int substringPosition(char* string, char* substring, int start)
+{
+	int n = getLength(string),
+		m = getLength(substring);
+	if (m > n)
+	{
+		return -1;
+	}
+	int count = 0, index = -1;
+	for (int i = start; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (substring[j] == string[i + count])
+			{
+				index = i;
+				count++;
+				if (j == m - 1)
+				{
+					return index;
+				}
+				else
+				{
+					continue;
+				}
+			}
+			else
+			{
+				index = -1;
+				count = 0;
+				break;
+			}
+		}
+	}
+	return index;
+}
+
+void stringReplacement(char* string, char* toBeReplaced, char* replace, char* newString)
+{
+
+	int newLength = getLength(replace), oldLength = getLength(toBeReplaced), sLength = getLength(string), start = 0, oldStart = 0, count = 0, difference = newLength - oldLength;
+	int index = substringPosition(string, toBeReplaced, start);
+	for (int i = 0; i < index; i++)
+	{
+		newString[i] = string[i];
+	}
+	while (index != -1)
+	{
+		for (int j = index + count*(difference) , i = 0; j < index + newLength + count*(difference); j++, i++)
+		{
+			newString[j] = replace[i];
+		}
+		start = index + newLength;
+		int newIndex = substringPosition(string, toBeReplaced, start);
+		while (newIndex != -1)
+		{
+			for (int i = index + newLength + count * (difference), j = index + oldLength; j < newIndex; i++, j++)
+			{
+				newString[i] = string[j];
+			}
+			break;
+		}
+		while (newIndex == -1)
+		{
+			for (int i = index + newLength +count*(difference) , j = index + oldLength; j < sLength; i++, j++)
+			{
+				newString[i] = string[j];
+			}
+			break;
+		}
+
+		index = substringPosition(string, toBeReplaced, start);
+		count++;
+	}
+}
+
+void displayArray(char* string)
+{
+	for (int i = 0; i < getLength(string); i++)
+	{
+		cout << "string[" << i + 1 << "] = " << string[i] << endl;
 	}
 }
