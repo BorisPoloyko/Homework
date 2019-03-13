cb767c6769b80f12ea05473d6bfde4c57acba002
BorisPoloyko <43009854+BorisPoloyko@users.noreply.github.com>
2019-03-13 09:46:46 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -8,141 +8,165 @@ typedef double(*operation)(double**,int);
 typedef bool(*condition)(double**, double**, int, operation);
 
 int inputSize();
-double taylor(int, int);
+double formula(int, int);
+double taylor(int , int);
+double msin(double, double);
 double** defineSquareMatrix(int);
 void displaySquareMatrix(double**, int);
 double** buildSquareMatrix(double**,int, matrixType);
+double** buildSquareMatrixTaylor(double**,int);
 double** buildRandomMatrix(double**, int);
 double maxSumColumn(double**, int n);
 void compareSquareMatrix(double**, double**, int n, operation, condition);
 bool ifFirstMatrixGreater(double**, double**, int, operation);
 
+double acc = 0.1;
 
 int main()
 {
-	int n = inputSize();
-
-	double** matrixA = defineSquareMatrix(n);
-
-	matrixA = buildSquareMatrix(matrixA, n, taylor);
-
-	displaySquareMatrix(matrixA, n);
-
-	double** matrixB = defineSquareMatrix(n);
-
-	matrixB = buildRandomMatrix(matrixB, n);
-
-	cout << endl;
-
-	displaySquareMatrix(matrixB, n);
-
-	compareSquareMatrix(matrixA, matrixB, n, maxSumColumn, ifFirstMatrixGreater);
-
-	system("pause");
-
-	return 0;
+    int n = inputSize();
+    
+    double** matrixA = defineSquareMatrix(n);
+    
+    matrixA = buildSquareMatrix(matrixA, n, formula);
+    
+    displaySquareMatrix(matrixA, n);
+    
+    double** matrixB = defineSquareMatrix(n);
+    
+    matrixB = buildSquareMatrix(matrixA, n, taylor);
+    
+    cout << endl;
+    
+    displaySquareMatrix(matrixB, n);
+    
+    compareSquareMatrix(matrixA, matrixB, n, maxSumColumn, ifFirstMatrixGreater);
+    
+    system("pause");
+    
+    return 0;
 }
 
 bool ifFirstMatrixGreater(double** matrixA, double** matrixB, int n, operation operation)
 {
-	return operation(matrixA, n) > operation(matrixB, n);
+    return operation(matrixA, n) > operation(matrixB, n);
 }
 
 int inputSize()
 {
-	int n;
-	while (true)
-	{
-		cout << "Please enter the size of the matrix, n = ";
-		cin >> n;
-		if (n > 0)
-		{
-			return n;
-		}
-		cout << "Invalid data! Try again, n =  ";
-	}
+    int n;
+    while (true)
+    {
+        cout << "Please enter the size of the matrix, n = ";
+        cin >> n;
+        if (n > 0)
+        {
+            return n;
+        }
+        cout << "Invalid data! Try again, n =  ";
+    }
 }
 
 double** defineSquareMatrix(int n)
 {
-	double** matrix = new double*[n];
-	for (int i = 0; i < n; ++i)
-	{
-		matrix[i] = new double[n];
-	}
-	return matrix;
+    double** matrix = new double*[n];
+    for (int i = 0; i < n; ++i)
+    {
+        matrix[i] = new double[n];
+    }
+    return matrix;
 }
 
 void displaySquareMatrix(double** matrix, int n)
 {
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cout.width(15);
-			cout << *(*(matrix + i) + j);
-		}
-		cout << endl;
-	}
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout.width(15);
+            cout << *(*(matrix + i) + j);
+        }
+        cout << endl;
+    }
 }
 
-double** buildSquareMatrix(double** matrix,int n, matrixType type)
+double** buildSquareMatrix(double** matrix,int n, matrixType formula)
 {
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			*(*(matrix + i) + j) = type(i, j);
-		}
-	}
-	return matrix;
-
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            *(*(matrix + i) + j) = formula(i, j);
+        }
+    }
+    return matrix;
+    
 }
 
 double** buildRandomMatrix(double** matrix, int n)
 {
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			*(*(matrix + i) + j) = ((rand() % 15) - 5) / (double)(rand() % 10);
-		}
-	}
-	return matrix;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            *(*(matrix + i) + j) = ((rand() % 15) - 5) / (double)(rand() % 10);
+        }
+    }
+    return matrix;
+}
+
+double formula(int i, int j)
+{
+    return (i == j) ? i : (sin(2 * i*j) + sin(i)) / ((i - j - 4) ^ 3 + (i + j) ^ 2);
 }
 
 double taylor(int i, int j)
 {
-	return (i == j) ? i : (sin(2 * i*j) + sin(i)) / ((i - j - 4) ^ 3 + (i + j) ^ 2);
+    return (i == j) ? i : (msin(2 * i*j, acc) + msin(i, acc)) / ((i - j - 4) ^ 3 + (i + j) ^ 2);
 }
+
 
 double maxSumColumn(double** matrix, int n)
 {
-	
-	double maxSum = 0;
-	for (int j = 0; j < n; j++)
-	{
-		double sum = 0;
-		for (int i = 0; i < n; i++)
-		{
-			sum += *(*(matrix + i) + j);
-		}
-		if (sum > maxSum)
-		{
-			maxSum = sum;
-		}
-	}
-	return maxSum;
+    
+    double maxSum = 0;
+    for (int j = 0; j < n; j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += *(*(matrix + i) + j);
+        }
+        if (sum > maxSum)
+        {
+            maxSum = sum;
+        }
+    }
+    return maxSum;
 }
 
 void compareSquareMatrix(double** matrixA, double** matrixB,int n,operation operation, condition condition)
 {
-	if (condition(matrixA, matrixB, n, operation))
-	{
-		cout << "The first matrix is greater! " << endl;
-	}
-	else
-	{
-		cout << "The second matrix is greater! " << endl;
-	}
+    if (condition(matrixA, matrixB, n, operation))
+    {
+        cout << "The first matrix is greater! " << endl;
+    }
+    else
+    {
+        cout << "The second matrix is greater! " << endl;
+    }
+}
+
+double msin(double from, double accuracy)
+{
+    double sin = 0, term = from;
+    int i = 1;
+    while (fabs(term)>accuracy)
+    {
+        sin += term;
+        term *= -1.0*(from*from) / (2 * i) / (2 * i + 1);
+        i++;
+    }
+    
+    return sin;
 }
